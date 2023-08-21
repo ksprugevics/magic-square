1e9470c21d4cd48d8082986aed51f00e0965a25c
klavssprugevics <klavs.sprugevics@gmail.com>
2023-08-21 20:41:08 +0300

    First iteration of singly even magic square solve

--- magic_square.py
+++ magic_square.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 def calculateMagicConstant(size):
     return size * (size * size + 1) / 2
 
-
+# Shuffle until we get a magic square (infinitely slow) but on average took ~100k iterations
 def bogoSolveMagicSquare(size):
     numbers = np.arange(1, size * size + 1)
     iterations = 0
@@ -49,8 +50,8 @@ def positionCalcuateSolveMagicSquare(size):
 
 # https://www.geeksforgeeks.org/magic-square/
 # Only for odd sized magic squares
-def vedicMathsSolveMagicSquare(size):
-    numbers = np.arange(1, size * size + 1)
+def vedicMathsSolveMagicSquare(size, startNumber):
+    numbers = np.arange(startNumber, startNumber + size * size)
     magicSquare = np.zeros((size, size))
     elementPos = [int(size / 2), size - 1]
     magicSquare[elementPos[0], elementPos[1]] = numbers[0]
@@ -69,8 +70,8 @@ def vedicMathsSolveMagicSquare(size):
 
         magicSquare[elementPos[0], elementPos[1]] = numbers[i]
 
-    print("Found magic square with magic constant: " + str(calculateMagicConstant(size)) + " validated: " + str(validation(magicSquare.flatten(), size)))
-    print(magicSquare)
+    # print("Found magic square with magic constant: " + str(calculateMagicConstant(size)) + " validated: " + str(validation(magicSquare.flatten(), size)))
+    # print(magicSquare)
     return magicSquare
 
 # https://www.1728.org/magicsq2.htm
@@ -99,6 +100,44 @@ def doublyEvenMagicSquareSolve(size):
     print(magicSquare)
     return magicSquare
 
+
+# https://www.1728.org/magicsq3.htm
+def singlyEvenMagicSquareSolve(size):
+    if size % 4 == 0 or size % 2 != 0 or size == 2:
+        print("Size must be divisble by 2 but not by 4 and should be larger than 2")
+        return
+    
+    magicSquare = np.zeros((size,size))
+
+    subSquareSize = int(size / 2)
+    sub1 = vedicMathsSolveMagicSquare(subSquareSize, 1)
+    sub2 = vedicMathsSolveMagicSquare(subSquareSize, 1 + 1 * subSquareSize * subSquareSize)
+    sub3 = vedicMathsSolveMagicSquare(subSquareSize, 1 + 2 * subSquareSize * subSquareSize)
+    sub4 = vedicMathsSolveMagicSquare(subSquareSize, 1 + 3 * subSquareSize * subSquareSize)
+    magicSquare = np.vstack((np.hstack((sub1, sub3)), np.hstack((sub4, sub2))))
+    print(magicSquare)
+
+    temp = magicSquare[0, 0]
+    magicSquare[0, 0] = magicSquare[3, 0]
+    magicSquare[3, 0] = temp
+
+    temp = magicSquare[2, 0]
+    magicSquare[2, 0] = magicSquare[5, 0]
+    magicSquare[5, 0] = temp
+
+    temp = magicSquare[1, 1]
+    magicSquare[1, 1] = magicSquare[4, 1]
+    magicSquare[4, 1] = temp
+
+    temp = np.hstack((magicSquare[:3, 5], magicSquare[3:, 5]))
+    magicSquare[:, 5] = temp
+    print(validation(magicSquare, size))
+
+    # print("Found magic square with magic constant: " + str(calculateMagicConstant(size)) + " validated: " + str(validation(magicSquare.flatten(), size)))
+    print(magicSquare)
+    return magicSquare
+
+
 def validation(numbers, size):
     magicConstant = calculateMagicConstant(size)
     arr = np.reshape(np.copy(numbers), (-1, size))
@@ -112,5 +151,6 @@ def validation(numbers, size):
 
 # bogoSolveMagicSquare(3)
 # positionCalcuateSolveMagicSquare(5)
-# vedicMathsSolveMagicSquare(7)
-doublyEvenMagicSquareSolve(8)
+# vedicMathsSolveMagicSquare(3, 10)
+# doublyEvenMagicSquareSolve(8)
+singlyEvenMagicSquareSolve(6)
